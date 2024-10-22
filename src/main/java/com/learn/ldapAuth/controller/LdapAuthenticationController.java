5ad024d92ea1b0005eced2dafbbeed409d9bb775
Talented Developer <40318152+talenteddeveloper@users.noreply.github.com>
2024-10-22 20:59:53 +0530

    Update LdapAuthenticationController.java

--- src/main/java/com/learn/ldapAuth/controller/LdapAuthenticationController.java
+++ src/main/java/com/learn/ldapAuth/controller/LdapAuthenticationController.java
@@ -3,6 +3,8 @@ package com.learn.ldapAuth.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// youtube playlist ldap: https://www.youtube.com/playlist?list=PLyzY2l387AlO0KMY2Ydkzyxj1NgwtvR64
+//  udemy ldap course link : https://www.udemy.com/share/10c2jr3@gXg1kvyeuFFQapv4E2gw0HJSi9gzWhNBOhYpOZd6zC04bgBjd4p9a3r-yUv8fEzoEg==/
 @RestController
 public class LdapAuthenticationController {
 	 @GetMapping("/")
